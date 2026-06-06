ec5a7a74e8dac23d61dca4bd4183edc5cc6cbb96
UBhagya <nethmini256@gmail.com>
2026-06-06 18:13:34 +0530

    Setup Spring Boot backend structure

---
+++ src/main/java/com/houseplanner/service/UserService.java
@@ -0,0 +1,7 @@
+package com.houseplanner.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
---
+++ src/test/java/com/houseplanner/HouseplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.houseplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
